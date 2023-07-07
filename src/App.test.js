8f6c5fb9af71ec6e71fac69f2dee57ec51d80133
timDeHof <this.is.tim.dehof@gmail.com>
2023-07-06 21:16:13 -0400

    chore(.gitignore): update .gitignore to exclude amplify files and directories feat(.vscode/settings.json): add .vscode/settings.json file to exclude amplify files and directories in VS Code feat(amplify/.config/project-config.json): add amplify/.config/project-config.json file feat(amplify/backend/auth/amplifyreactgraphqln21dd42e6/cli-inputs.json): add amplify/backend/auth/amplifyreactgraphqln21dd42e6/cli-inputs.json file feat(amplify/backend/backend-config.json): add amplify/backend/backend-config.json file feat(amplify/backend/tags.json): add amplify/backend/tags.json file feat(amplify/backend/types/amplify-dependent-resources-ref.d.ts): add amplify/backend/types/amplify-dependent-resources-ref.d.ts file feat(amplify/cli.json): add amplify/cli.json file feat(amplify/team-provider-info.json): add amplify/team-provider-info.json file chore

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import React from "react";
 
-test('renders learn react link', () => {
-  render(<App />);
+test("renders learn react link", () => {
+  render(<App signOut={undefined} />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
